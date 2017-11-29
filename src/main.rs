88d056df533632394793a95e37580129a9e7aec7
Aaron Malone <aaron@munge.net>
2017-11-28 20:18:21 -0600

    working 1.20 w/rayon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-extern crate rustc_serialize;
-use rustc_serialize::json::Json;
+extern crate rayon;
+extern crate serde_json;
+
+use serde_json::Value;
+use rayon::prelude::*;
 use std::process::Command;
 use std::path::Path;
 use std::error::Error;
@@ -7,28 +10,32 @@ use std::fs;
 
 fn main() {
     let path = &Path::new(".");
-    for entry in fs::read_dir(path).unwrap() {
-        let path = entry.unwrap().path();
-        let filename = path.to_str().unwrap();
-        match is_over_720p(&filename) {
-            Ok(true) => println!("{}", filename),
-            _ => (),
-        }
+    let entries: Vec<_> = fs::read_dir(path).unwrap().collect();
+    let over: Vec<_> = entries
+        .par_iter()
+        .filter_map(|entry| {
+            let fname = entry.as_ref().unwrap().path().to_str().unwrap().to_owned();
+            match is_over_720p(&fname) {
+                Ok(true) => Some(fname),
+                _ => None,
+            }
+        })
+        .collect();
+    for fname in over {
+        println!("{}", fname);
     }
 }
 
 fn is_over_720p(filename: &str) -> Result<bool, Box<Error>> {
     let args = &["-v", "quiet", "-of", "json", "-show_streams", filename];
-    let output = try!(Command::new("ffprobe").args(args).output());
-    let stdout = try!(std::str::from_utf8(&output.stdout));
-    if let Ok(probe_json) = Json::from_str(stdout) {
-        if let Some(&Json::Array(ref streams)) = probe_json.find("streams") {
-            for stream in streams {
-                if let Some(&Json::U64(height)) = stream.find("height") {
-                    return Ok(height > 700);
-                }
-            }
-        }
+    let output = Command::new("ffprobe").args(args).output()?;
+    let stdout = std::str::from_utf8(&output.stdout)?;
+    let probe: Value = serde_json::from_str(stdout)?;
+    match probe["streams"] {
+        Value::Array(ref streams) => Ok(streams.iter().any(|s| match s["height"] {
+            Value::Number(ref n) => n.as_i64().unwrap() > 700,
+            _ => false,
+        })),
+        _ => Ok(false),
     }
-    Ok(false)
 }

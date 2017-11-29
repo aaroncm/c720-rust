9046f3d0a0d98a0ef83c6b2f8ab58f5ae9e1d51e
Aaron Malone <aaron@munge.net>
2017-11-28 20:31:04 -0600

    add indicatif progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+extern crate indicatif;
 extern crate rayon;
 extern crate serde_json;
 
 use serde_json::Value;
 use rayon::prelude::*;
+use indicatif::ProgressBar;
 use std::process::Command;
 use std::path::Path;
 use std::error::Error;
@@ -11,16 +13,20 @@ use std::fs;
 fn main() {
     let path = &Path::new(".");
     let entries: Vec<_> = fs::read_dir(path).unwrap().collect();
+    let pbar = ProgressBar::new(entries.len() as u64);
     let over: Vec<_> = entries
         .par_iter()
         .filter_map(|entry| {
             let fname = entry.as_ref().unwrap().path().to_str().unwrap().to_owned();
-            match is_over_720p(&fname) {
+            let result = is_over_720p(&fname);
+            pbar.inc(1);
+            match result {
                 Ok(true) => Some(fname),
                 _ => None,
             }
         })
         .collect();
+    pbar.finish();
     for fname in over {
         println!("{}", fname);
     }
@@ -33,7 +39,7 @@ fn is_over_720p(filename: &str) -> Result<bool, Box<Error>> {
     let probe: Value = serde_json::from_str(stdout)?;
     match probe["streams"] {
         Value::Array(ref streams) => Ok(streams.iter().any(|s| match s["height"] {
-            Value::Number(ref n) => n.as_i64().unwrap() > 700,
+            Value::Number(ref n) => n.as_i64().unwrap() > 720,
             _ => false,
         })),
         _ => Ok(false),
